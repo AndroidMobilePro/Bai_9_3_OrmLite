8f023e8b751b8be6af3360d32257947e28a83044
at-cuongnv <vancuong.dev@gmail.com>
2018-07-11 19:48:45 +0700

    This is OrmLite DB

---
+++ app/src/main/java/com/proteam/bai_9_3_ormlite/Note.java
@@ -0,0 +1,43 @@
+package com.proteam.bai_9_3_ormlite;
+
+import com.orm.SugarRecord;
+
+public class Note extends SugarRecord {
+    String title, note;
+    long time;
+
+    public Note() {
+    }
+
+
+    public Note(String title, String note, long time) {
+        this.title = title;
+        this.note = note;
+        this.time = time;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+}
